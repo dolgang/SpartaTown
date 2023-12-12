4694cfee55ab502f348b91af82ed82b9e1b67536
dolgang <149345680+dolgang@users.noreply.github.com>
2023-12-12 16:23:40 +0900

    [1212] 게임 루틴 추가
    - 간단한 게임 로직 구현

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,12 +11,120 @@ public class GameManager : MonoBehaviour
 
     public Transform Player {  get; private set; }
     [SerializeField] private string playerTag = "Player";
+    private HealthSystem playerHealthSystem;
 
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
+
+    [SerializeField] private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0;
+
+    public float spawnInterval = 0.5f;
+    public List<GameObject> enemyPrefabs = new List<GameObject>();
+
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPositions = new List<Transform>();
 
     private void Awake()
     {
         instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
+
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHeathUI;
+        playerHealthSystem.OnHeal += UpdateHeathUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
+
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPositions.Add(spawnPositionsRoot.GetChild(i));
+        }
     }
 
+    private void Start()
+    {
+        StartCoroutine("StartNextWave");
+    }
+
+    IEnumerator StartNextWave()
+    {
+        while (true)
+        {
+            if(currentSpawnCount == 0)
+            {
+                UpdateWaveUI();
+                yield return new WaitForSeconds(2f);
+
+                if(currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPositions.Count ? waveSpawnPosCount : waveSpawnPosCount + 1;
+                    waveSpawnCount = 0;
+                }
+
+                if(currentWaveIndex % 5 == 0)
+                {
+
+                }
+
+                if(currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+                for(int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPositions.Count);
+                    for(int j = 0; j < waveSpawnCount; j++)
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefabs.Count);
+                        GameObject enemy = Instantiate(enemyPrefabs[prefabIdx], spawnPositions[posIdx].position, Quaternion.identity);
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath;
+                        //enemy.GetComponent<CharacterStatsHandler>
+                        currentSpawnCount++;
+                        yield return new WaitForSeconds(spawnInterval);
+                    }
+                }
+
+                currentWaveIndex++;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+        StopAllCoroutines();    // 오브젝트 내에서 하나만 돌고 있으니 걍 다 멈추게 던짐
+    }
+
+    private void UpdateHeathUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void UpdateWaveUI()
+    {
+        waveText.text = (currentWaveIndex + 1).ToString();
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExtiGame()
+    {
+        Application.Quit();
+    }
 }
